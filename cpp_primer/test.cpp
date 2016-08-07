dcaadfcbf7ffcc19c05b41859431871aa97d2fa4
ObjectNotFound <zhaoxiaodong0902@163.com>
2016-08-07 17:10:57 +0800

    3.4 Introducing Iterators $

--- cpp_primer/test.cpp
+++ cpp_primer/test.cpp
@@ -1,15 +1,32 @@
 #include <iostream>
+#include <vector>
+#include <iterator>
 #include <string>
 using namespace std;
 
 int main()
 {
-	string s = "Hello, world!!!";
-	string result;
-	for (char c : s)
-		if (!ispunct(c))
-			result += c;
-	cout << result;
+	vector<int> a{1,2,3,4,5,6,7,8,9,10};
+	int target = 0;
+	auto begin = a.begin(), end = a.end();
+	auto mid = begin + (end - begin) / 2;
+	while (begin != end)
+	{
+		if (*mid > target)
+		{
+			end = mid;
+		}
+		else if (*mid < target)
+		{
+			begin = mid;
+		}
+		else
+			break;
+		mid = begin + (end - begin) / 2;
+	}
+	if (begin == end)
+		cout << "not found";
+	cout << *mid;
 	system("pause");
 	return 0;
 }
