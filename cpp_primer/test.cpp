382dc33886358b1e93a3cc9677ffdd7a35ca2814
zhaoxiaodong <zhaoxiaodong@DESKTOP-8LF1U3N>
2016-08-09 10:15:45 +0800

    with cpp primer.pdf add

--- cpp_primer/test.cpp
+++ cpp_primer/test.cpp
@@ -6,27 +6,13 @@ using namespace std;
 
 int main()
 {
-	vector<int> a{1,2,3,4,5,6,7,8,9,10};
-	int target = 0;
-	auto begin = a.begin(), end = a.end();
-	auto mid = begin + (end - begin) / 2;
-	while (begin != end)
-	{
-		if (*mid > target)
-		{
-			end = mid;
-		}
-		else if (*mid < target)
-		{
-			begin = mid;
-		}
-		else
-			break;
-		mid = begin + (end - begin) / 2;
-	}
-	if (begin == end)
-		cout << "not found";
-	cout << *mid;
+	int a[5] = { 1,2,3,4,5 };
+	int *beg = begin(a);
+	int *pend = end(a);
+	int size = pend - beg;
+	cout << size << endl
+		<< *(pend - 1) << endl
+		<< *(pend - 1);
 	system("pause");
 	return 0;
 }
